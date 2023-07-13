4a70f05d6b06a1c879b89016bc6507a5289fc8d9
cyberparty <jc@cyberparty.me>
2023-07-14 00:24:42 +0100

    fixed syntax error

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let mut board: Board = Board::new();
     board.load_from_file("src/plots_1.txt");
     println!("{}", board);
-    println!("{:?}", board.get_neighbours((2, 1)))
+    println!("{:?}", board.get_neighbours((2, 1)));
     board.load_from_file("src/plots_2.txt");
     println!("{}", board);
-    println!("{:?}", board.get_neighbours((2, 1)))
+    println!("{:?}", board.get_neighbours((2, 1)));
 }
